bc23f5a3834323b51de87418fafc415ea3341ad0
Luzarin <lucas.blachet@uc.cl>
2025-10-01 20:52:32 -0300

    up

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.navlinks a, .logo a').forEach(function(link) {
     link.addEventListener('mouseenter', function() {
-        link.style.color = '#00ff88'; // Color al pasar el mouse
+        link.style.color = '#86c4ebff'; // Color al pasar el mouse
     });
     link.addEventListener('mouseleave', function() {
         link.style.color = ''; // Vuelve al color original (usa el CSS)
@@ -55,4 +55,4 @@ document.addEventListener('click', (e) => {
       e.preventDefault();
     }
   }
-});
+}); 
